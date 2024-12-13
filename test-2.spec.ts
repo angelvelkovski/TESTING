7ad86a1b34a37d699dafc097e3297fe12d763a0a
Angel Velkovski <angelvelkovski1@gmail.com>
2024-12-13 17:40:55 +0100

    Update

--- test-2.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://demoqa.com/');
-  await page.locator('div').filter({ hasText: /^Alerts, Frame & Windows$/ }).nth(1).click();
-  await page.locator('li').filter({ hasText: 'Alerts' }).click();
-  await page.locator('div').filter({ hasText: /^Click me$/ }).nth(2).click();
-  await page.locator('#confirmButton').click();
-});
